eb11af1169b2c29e4458d7106b6ec98022439015
IsaBella Sztyber <69774341+bvalient@users.noreply.github.com>
2022-04-18 13:28:30 -0400

    flips updated

--- flips.js
+++ flips.js
@@ -1,16 +1,16 @@
 // Import the coinFlip function from your coin.mjs file
 import {coinFlips, countFlips} from './modules/coin.mjs';
-import minimist from 'minimist';
+import { createRequire } from 'module';
+
 
 // Call the coinFlip function and put the return into STDOUT
+const require = createRequire(import.meta.url);
+const args = require('minimist')(process.argv.slice(2));
+args['number']
 
-const args = minimist(process.argv.slice(2));
-args['count']; 
-
-const count = args.count || 1;
-let array = coinFlips(args.number);
+// const number = args.number || 1;
+let array = coinFlips(args.number || 1);
 
 console.log(array);
 console.log(countFlips(array));
-
 // npm install minimist
